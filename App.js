5238e1e7532fbb923a15d2a4197de13aff99da70
Omer-Cicek <omercicek160408@gmail.com>
2022-02-24 17:14:20 +0300

    plus operator added

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-//
-//
-//
-//
-//
-//
 const subtotal = document.getElementById('cart-subtotal');
-console.log(subtotal);
+const arr1 = [];
+const arr2 = [];
+const arr3 = [];
+const ömer = document.querySelectorAll('.product-info');
 
+//AddEventListener
 document.addEventListener('click', (e) => {
-  console.log(
-    e.target.parentElement.parentElement.querySelector('p').innerText
-  );
   if (e.target.classList.contains('remove-product')) {
     e.target.closest('.product').remove();
   }
@@ -28,4 +23,12 @@ document.addEventListener('click', (e) => {
       return;
     }
   }
+  ömer.forEach((infocard, i) => {
+    let countItem = infocard.querySelector('#product-quantity').innerText;
+    let priceItem = infocard
+      .getElementsByTagName('p')[0]
+      .innerText.split(' ')[0];
+    let productTotal = infocard.querySelector('.product-line-price');
+    productTotal.innerText = (countItem * priceItem).toFixed(2);
+  });
 });
